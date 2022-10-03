4e26746969a970268df4887f29b4bfe1901e2f33
zhuxianzhong <22525482@qq.com>
2022-10-03 13:49:45 +0800

    只出现一次的最大数字

---
+++ 0619-只出现一次的最大数字/Solution.sql
@@ -0,0 +1,13 @@
+SELECT
+    MAX(num) AS num
+FROM
+    (
+        SELECT
+            num
+        FROM
+            MyNumbers
+        GROUP BY
+            num
+        HAVING
+            COUNT(num) = 1
+    ) AS t;
