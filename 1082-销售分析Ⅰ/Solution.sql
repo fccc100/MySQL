cfbc7a6fc37261af1c2518e2ca3837bc333d8a2e
zhuxianzhong <22525482@qq.com>
2022-10-07 21:19:54 +0800

    销售分析Ⅰ

---
+++ 1082-销售分析Ⅰ/Solution.sql
@@ -0,0 +1,16 @@
+SELECT
+    seller_id
+FROM
+    Sales
+GROUP BY
+    seller_id
+HAVING
+    SUM(price) >= 
+    ALL(
+        SELECT
+            SUM(price)
+        FROM
+            Sales
+        GROUP BY
+            seller_id
+    );
